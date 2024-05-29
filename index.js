08f6b13bda5b3490ecb008fa6d7d6f1ee210bfc4
Dheeru12503 <142683911+Dheeru12503@users.noreply.github.com>
2024-05-30 04:06:24 +0530

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 // Load data from JSON files
 const saleOrders = JSON.parse(
